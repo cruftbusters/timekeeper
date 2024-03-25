0a4e2471b6379ff1abd601a97a88784a1daec289
arctair <jtyler_@outlook.com>
2024-03-25 10:22:49 -0700

    refactor

--- tests/punch.rs
+++ tests/punch.rs
@@ -6,25 +6,22 @@ use maud::html;
 async fn punch_in_punch_out() -> Result<(), reqwest::Error> {
     let default_url = String::from("http://localhost:3000");
     let url = env::var("url").unwrap_or(default_url);
-    let request_url = format!("{url}/punch");
     let client = reqwest::Client::new();
 
-    let body = client.post(&request_url)
+    let body = client.post(format!("{url}/punch"))
         .send()
         .await?
         .text()
         .await?;
 
     let expected_markup = html! { "punched in" };
-
     assert_eq!(body, expected_markup.into_string());
 
-    let body = client.post(request_url)
+    let body = client.post(format!("{url}/punch"))
         .send()
         .await?
         .text()
         .await?;
-
     let expected_markup = html! { "punched out" };
 
     assert_eq!(body, expected_markup.into_string());
