0ebf11e2afb8c53964175e6a730b8d8ea2db2b6c
arctair <jtyler_@outlook.com>
2024-03-25 00:07:19 -0700

    toggle punch sticks

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
+use std::sync::{Arc, Mutex};
 use axum::{
     http::StatusCode,
     Json,
     Router, routing::{get, post},
 };
+use axum::extract::State;
 use maud::{DOCTYPE, html, Markup};
 use serde::{Deserialize, Serialize};
+
+#[derive(Clone)]
+struct AppState {
+    punched_in: Arc<Mutex<bool>>,
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    let state = AppState { punched_in: Arc::new(Mutex::new(false)) };
+
     let app = Router::new()
         .route("/", get(index))
         .route("/punch", post(punch))
         .route("/heartbeat", get(heartbeat))
-        .route("/users", post(create_user));
+        .route("/users", post(create_user))
+        .with_state(state);
 
     let address = "[::]:3000";
     let listener = tokio::net::TcpListener::bind(address).await.unwrap();
@@ -31,8 +41,13 @@ async fn index() -> Markup {
     }
 }
 
-async fn punch() -> Markup {
-    html! { "punched in" }
+async fn punch(
+    State(state): State<AppState>,
+) -> Markup {
+    let mut punched_in = state.punched_in.lock().expect("mutex was poisoned");
+    *punched_in = !*punched_in;
+    let text = if *punched_in { "punched in" } else { "punched out" };
+    html! { (text) }
 }
 
 async fn heartbeat() -> Markup {
---
+++ tests/punch.rs
@@ -0,0 +1,33 @@
+use std::env;
+
+use maud::html;
+
+#[tokio::test]
+async fn punch_in_punch_out() -> Result<(), reqwest::Error> {
+    let default_url = String::from("http://localhost:3000");
+    let url = env::var("url").unwrap_or(default_url);
+    let request_url = format!("{url}/punch");
+    let client = reqwest::Client::new();
+
+    let body = client.post(&request_url)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    let expected_markup = html! { "punched in" };
+
+    assert_eq!(body, expected_markup.into_string());
+
+    let body = client.post(request_url)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    let expected_markup = html! { "punched out" };
+
+    assert_eq!(body, expected_markup.into_string());
+
+    Ok(())
+}
