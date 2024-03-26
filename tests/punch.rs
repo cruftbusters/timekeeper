df9ae28f27228bd9ecdff4b8c3d1d9bb98c5ee29
arctair <jtyler_@outlook.com>
2024-03-25 23:17:15 -0700

    assert status codes

--- tests/punch.rs
+++ tests/punch.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 use maud::html;
+use reqwest::StatusCode;
 
 #[tokio::test]
 async fn punch_in_punch_out() -> Result<(), reqwest::Error> {
@@ -8,42 +9,34 @@ async fn punch_in_punch_out() -> Result<(), reqwest::Error> {
     let url = env::var("url").unwrap_or(default_url);
     let client = reqwest::Client::new();
 
-    let body = client.get(format!("{url}/"))
-        .send()
-        .await?
-        .text()
-        .await?;
+    let response = client.get(format!("{url}/")).send().await?;
+    assert_eq!(response.status(), StatusCode::OK);
 
+    let body = response.text().await?;
     if !body.contains("punched out") {
         panic!("expected body to contain text '{}': \n{}", "punched out", body);
     }
 
-    let body = client.post(format!("{url}/punch"))
-        .send()
-        .await?
-        .text()
-        .await?;
+    let response = client.post(format!("{url}/punch")).send().await?;
+    assert_eq!(response.status(), StatusCode::OK);
 
+    let body = response.text().await?;
     let expected_markup = html! { "punched in" };
     assert_eq!(body, expected_markup.into_string());
 
-    let body = client.get(format!("{url}/"))
-        .send()
-        .await?
-        .text()
-        .await?;
+    let response = client.get(format!("{url}/")).send().await?;
+    assert_eq!(response.status(), StatusCode::OK);
 
+    let body = response.text().await?;
     if !body.contains("punched in") {
         panic!("expected body to contain text '{}': \n{}", "punched in", body);
     }
 
-    let body = client.post(format!("{url}/punch"))
-        .send()
-        .await?
-        .text()
-        .await?;
-    let expected_markup = html! { "punched out" };
+    let response = client.post(format!("{url}/punch")).send().await?;
+    assert_eq!(response.status(), StatusCode::OK);
 
+    let body = response.text().await?;
+    let expected_markup = html! { "punched out" };
     assert_eq!(body, expected_markup.into_string());
 
     Ok(())
